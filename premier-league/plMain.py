240338884d4e0b90d236cdc0054f470280322f4a
Hidalgo71 <y.ozdemir71@gmail.com>
2020-06-12 01:22:19 +0300

    Updated.

--- premier-league/plMain.py
+++ premier-league/plMain.py
@@ -1,3 +1,7 @@
 import numpy as np
 import pandas as pd
-import matplo
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+eplData = pd.read_csv("E:\PycharmProjects\premierLeague\premier-league\stats.csv")
+print(eplData.head(10))
